03fc4e6c4d58c3e9be594850f5644a2f39b091f3
mbrie041 <mbrie041@gmail.com>
2021-09-09 22:41:29 -0400

    Implement counter service

---
+++ src/app/counter.services.ts
@@ -0,0 +1,13 @@
+export class CounterService {
+  activeToInactiveCounter = 0;
+  InactiveToActiveCounter = 0;
+
+  incrementActiveToInactive() {
+    this.activeToInactiveCounter++;
+    console.log(this.activeToInactiveCounter);
+  }
+  incrementInactiveToActive() {
+    this.InactiveToActiveCounter++;
+    console.log(this.InactiveToActiveCounter);
+  }
+}
